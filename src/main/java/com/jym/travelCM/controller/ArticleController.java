85797882b29edbad805fececf65e8a89cd07475f
jjymj <https://github.com/youngmin30/webpj-2022-.git>
2022-03-11 10:10:40 +0900

    22 게시물 디테일 구현

--- src/main/java/com/jym/travelCM/controller/ArticleController.java
+++ src/main/java/com/jym/travelCM/controller/ArticleController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.Principal;
@@ -55,6 +56,20 @@ public class ArticleController {
         }
 
         return "redirect:/";
+
+    }
+
+    @GetMapping("/articles/{id}")
+    public String showDetail(@PathVariable(name = "id") Long id, Model model){
+
+        try {
+            ArticleDTO findArticle = articleService.getArticle(id);
+            model.addAttribute("article", findArticle);
+
+            return "usr/article/detail";
+        }catch (Exception e){
+            return "redirect:/";
+        }
 
     }
 
